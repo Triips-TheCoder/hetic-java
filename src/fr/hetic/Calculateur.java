3798459e775804bb4a30237f445bbfa938d9e4ef
Julian Laballe <julian.laballe@unlikely.technology>
2024-04-09 17:54:51 +0200

    feat: make exercice 1

---
+++ src/fr/hetic/Calculateur.java
@@ -0,0 +1,34 @@
+public class Calculateur {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Usage: java Calculateur <numérique> <numérique> <opérateur>");
+            return;
+        }
+
+        try {
+            double num1 = Double.parseDouble(args[0]);
+            double num2 = Double.parseDouble(args[1]);
+            String operateur = args[2];
+
+            double resultat = calculer(num1, num2, operateur);
+            System.out.println("Résultat : " + resultat);
+        } catch (NumberFormatException e) {
+            System.out.println("Les deux premiers arguments doivent être des nombres.");
+        }
+    }
+
+    public static double calculer(double num1, double num2, String operateur) {
+        switch (operateur) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            default:
+                throw new IllegalArgumentException("Opérateur non valide : " + operateur);
+        }
+    }
+}
+
+
